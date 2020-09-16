51e6555b0b2119d2bd1f79861d54faa642757150
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-09-16 14:36:32 -0600

    Added second file upload

--- scRNA-seq-atlas/app.R
+++ scRNA-seq-atlas/app.R
@@ -13,11 +13,16 @@ ui <- fluidPage(
         sidebarPanel(
             
             # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
+            fileInput("file1", "Choose Matrix File",
                       multiple = FALSE,
                       accept = c("text/csv",
                                  "text/comma-separated-values,text/plain",
-                                 ".csv")),
+                                 ".csv", ".tsv")),
+            fileInput("file2", "Choose Metadata File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv", ".tsv")),
             
             tags$hr(),
             
@@ -27,6 +32,11 @@ ui <- fluidPage(
             
             # Horizontal line ----
             tags$hr(),
+            
+            # Input: Select separator ----
+            radioButtons("sep", "Separator",
+                         choices = c(Tab = "\t"),
+                         selected = ","),
             
             # Input: Select number of rows to display ----
             radioButtons("disp", "Display",
@@ -57,28 +67,42 @@ server <- function(input, output) {
         # or all rows if selected, will be shown.
         
         req(input$file1)
-        
+        req(input$file2)
         # when reading semicolon separated files,
         # having a comma separator causes `read.csv` to error
         tryCatch(
             {
-                df <- read.csv(input$file1$datapath,
+                df1 <- read.csv(input$file1$datapath,
                                header = input$header,
                                sep = input$sep,
                                quote = input$quote)
+                
+                df2 <- read.csv(input$file2$datapath,
+                                header = input$header,
+                                sep = input$sep,
+                                quote = input$quote)
             },
             error = function(e) {
                 # return a safeError if a parsing error occurs
                 stop(safeError(e))
             }
         )
-        
+        #file 1
         if(input$disp == "head") {
-            return(head(df))
+            return(head(df1))
         }
         else {
-            return(df)
+            return(df1)
         }
+        
+        #file 2
+        if(input$disp == "head") {
+            return(head(df2))
+        }
+        else {
+            return(df2)
+        }
+        
         
     })
     
